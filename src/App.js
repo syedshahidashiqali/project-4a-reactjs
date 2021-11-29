db3861a2f1949d42324ffcf3319731244ca2dd1b
Syed Shahid <syedshahid1215@gmail.com>
2021-11-29 16:25:11 +0500

    completed UI of the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
 
+// import images
+import birdImg from "./images/bird.gif";
+import cloudImg from './images/cloud.gif';
+import carImg from "./images/car.png";
+import wheelImg from "./images/wheel.png";
+
 function App() {
   return (
-    <div>
-      Hello, world
+    <div className="app">
+      <div className="animation">
+          <span className="state">Animation State: </span>
+          <button className="pause">Pause</button>
+          <button className="play">Play</button>
+          <button className="reverse">Reverse</button>
+          <button className="speedup">Speed Up</button>
+          <button className="speeddown">Speed Down</button>
+      </div>
+      <div className="bird-div">
+          <img className="bird" src={birdImg} alt="bird" />
+      </div>
+      <div className="cloud-div">
+          <img src={cloudImg} alt="cloud" className="cloud" />
+          <img src={cloudImg} alt="cloud" className="cloud" />
+          <img src={cloudImg} alt="cloud" className="cloud" />
+      </div>
+      <div className="city"></div>
+      <div className="highway"></div>
+      <div className="car-div">
+          <img src={carImg} alt="" className="car" />
+      </div>
+      <div className="wheels">
+          <img src={wheelImg} alt="" className="wheel back-wheel" />
+          <img src={wheelImg} alt="" className="wheel front-wheel" />
+      </div>
     </div>
   );
 }
